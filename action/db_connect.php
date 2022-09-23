2224c42f9eb22891269c51de1485a2b118e324df
yourusername <saleh_islam1990hotmail.com>
2022-09-23 14:13:33 +0200

    changes

---
+++ action/db_connect.php
@@ -0,0 +1,13 @@
+<?php
+$hostName = "127.0.0.1";
+$userName = "root";
+$password = "";
+$dbName = "CR10-Islam-Saleh-BigLibrary";
+
+
+$conn = mysqli_connect($hostName, $userName, $password, $dbName);
+
+if (!$conn) {
+    echo "error";
+    exit;
+}
